5c45c5b00b57744716b24a880ad1465b9760d900
restapiapp1 <91378522+restapiapp1@users.noreply.github.com>
2021-09-25 20:02:03 +0600

    Add files via upload

---
+++ restapi/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+
+from rest_framework import routers
+
+from .views import PersonViewSet, SpeciesViewSet
+
+router = routers.DefaultRouter()
+router.register(r'people', PersonViewSet)
+router.register(r'species', SpeciesViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
